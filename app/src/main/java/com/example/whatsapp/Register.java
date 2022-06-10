911afb2f33dd82849339527d37290c2570a528a3
maorh <maorha101@gmail.com>
2022-06-10 19:12:05 +0300

    login and signUp pages

--- app/src/main/java/com/example/whatsapp/Register.java
+++ app/src/main/java/com/example/whatsapp/Register.java
@@ -6,16 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button loginToSignup = findViewById(R.id.loginToSignup);
-        loginToSignup.setOnClickListener(v->{
-            Intent i = new Intent(this, Register.class);
+        setContentView(R.layout.activity_register);
+        Button signupToLogin = findViewById(R.id.signupToLogin);
+        signupToLogin.setOnClickListener(v->{
+            Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         });
     }

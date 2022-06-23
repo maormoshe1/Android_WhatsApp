66ec9950e42782a0abb2849e832d607464a351ef
maorh <maorha101@gmail.com>
2022-06-23 17:41:22 +0300

    different room for any client

--- app/src/main/java/com/example/whatsapp/pages/Chat.java
+++ app/src/main/java/com/example/whatsapp/pages/Chat.java
@@ -25,7 +25,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 public class Chat extends AppCompatActivity {
     private AppDB dbContact, dbChat;
